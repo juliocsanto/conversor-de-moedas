0f4f9ce4e89ebcb45ffeb06128b5ccfd4641b812
Julio Nascimento <juliocsanto@hotmail.com>
2021-05-13 10:49:08 -0300

    First commit Julio

--- app.js
+++ app.js
@@ -28,3 +28,5 @@
       - Para obter a key e fazer requests, você terá que fazer login e escolher
         o plano free. Seus dados de cartão de crédito não serão solicitados.
 */
+
+//"First Commit"
